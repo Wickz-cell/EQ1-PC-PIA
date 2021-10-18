12b9b17468c1e291a2129310765e293c60b6d448
MisaelLT <88629550+MisaelLT@users.noreply.github.com>
2021-10-18 14:42:37 -0500

    Update EscanerPuertos.ps1

--- core/EscanerPuertos.ps1
+++ core/EscanerPuertos.ps1
@@ -1,17 +1,13 @@
-﻿# Definimos un array con puertos a escanear
+# Definimos un array con puertos a escanear
 #Establecemos una variable para Waittime
 #
 $portstoscan = @(20,21,22,23,25,50,51,53,80,110,119,135,136,137,138,139)
 $waittime = 100
 
-# Solicitamos direccion ip a escanear:
+# Solicitamos la direccion ip a escanear
 #
-ipconfig >info.txt
-$dir = Select-String -Pattern " Direcci¢n IPv4. . . . . . . . . . . . . . : \d{3}.\d.\d.\d{3}"  info.txt
-$dire = $dir  -match '\d{3}.\d.\d.\d{3}'
-$direccion = $Matches.0
-
-Write-Host "Direccion ip a escanear: " $direccion
+Write-Host "Direccion ip a escanear: " -NoNewline
+$direccion = Read-Host
 
 #Generamos bucle foreach para evaluar cada puerto
 #
