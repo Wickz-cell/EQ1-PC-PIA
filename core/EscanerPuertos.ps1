209e8233190f7c9e1907ac2c9bbe08130a6abbf0
MisaelLT <88629550+MisaelLT@users.noreply.github.com>
2021-10-30 11:57:54 -0500

    Update EscanerPuertos.ps1

--- core/EscanerPuertos.ps1
+++ core/EscanerPuertos.ps1
@@ -1,22 +1,21 @@
+# Parametro para la direccion ip a escanear
+#
+param([Parameter(Mandatory)] [string]$direccion_ip)
+#
 # Definimos un array con puertos a escanear
-#Establecemos una variable para Waittime
+# Establecemos una variable para Waittime
 #
 $portstoscan = @(20,21,22,23,25,50,51,53,80,110,119,135,136,137,138,139)
 $waittime = 100
-
-# Solicitamos la direccion ip a escanear
 #
-Write-Host "Direccion ip a escanear: " -NoNewline
-$direccion = Read-Host
-
-#Generamos bucle foreach para evaluar cada puerto
+# Generamos bucle foreach para evaluar cada puerto
 #
 foreach ($p in $portstoscan)
 {
     $TpcObject = New-Object System.Net.Sockets.TcpClient
-        try{ $resultado = $TpcObject.ConnectAsync($direccion,$p).Wait($waittime)}catch{}
+        try{ $resultado = $TpcObject.ConnectAsync($direccion_ip,$p).Wait($waittime)}catch{}
         if ($resultado -eq "True")
         {
-            Write-Host "Puerto abierto: " -NoNewline; Write-Host $p -ForegroundColor Green
+            Write-Host "Puerto abierto:"$p
         }
 }
