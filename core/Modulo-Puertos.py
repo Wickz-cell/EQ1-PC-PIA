27eceac21f66a3871ca3d52ed2bd5c24ffd38590
Wickz-cell <91346012+Wickz-cell@users.noreply.github.com>
2021-11-07 17:40:20 -0600

    Delete core directory

--- core/Modulo-Puertos.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-
-comando = ("powershell -ExecutionPolicy ByPass -File EscanerPuertos.ps1")
-procesoPowerShell = subprocess.check_output(comando)
-print(procesoPowerShell.decode())
